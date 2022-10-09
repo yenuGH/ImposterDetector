04e0a36cafc94eb8aa18fef7afa8c2cb6f65dc58
Alex <metroid3d2@gmail.com>
2022-10-09 15:40:34 -0700

    Added documentation, stubbed functions for game model

--- app/src/main/java/ca/cmpt276/assignment3/activities/GameActivity.java
+++ app/src/main/java/ca/cmpt276/assignment3/activities/GameActivity.java
@@ -16,22 +16,37 @@ import android.widget.Toast;
 
 import ca.cmpt276.assignment3.R;
 
+/**
+ * Activity where the game is played
+ */
 public class GameActivity extends AppCompatActivity {
-    private static int NUM_ROWS = 4;
-    private static int NUM_COLUMNS = 5;
+    private static int NUM_ROWS = 6;
+    private static int NUM_COLUMNS = 15;
 
     Button[][] buttons = new Button[NUM_ROWS][NUM_COLUMNS];
+
+    private int numberOfGamesPlayed;
+    private int scans = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        numberOfGamesPlayed = 3;
+
         populateButtons();
-        updateText(5,42,0,0);
+        updateGameText(5,42, scans, numberOfGamesPlayed);
     }
 
-    private void updateText(int foundMines, int totalMines, int scansUsed, int timesPlayed) {
+    /**
+     * Update the game's text fields
+     * @param foundMines Number of mines the player has found
+     * @param totalMines Total number of mines on the game board
+     * @param scansUsed Number of scans the player has used
+     * @param timesPlayed Number of times the player has played a game
+     */
+    private void updateGameText(int foundMines, int totalMines, int scansUsed, int timesPlayed) {
         // Update the textview displaying the number of mines found/total
         String foundMinesString = String.format(getString(R.string.found_mines),foundMines,totalMines);
         updateTextView(R.id.tvFoundMines, foundMinesString);
@@ -55,6 +70,9 @@ public class GameActivity extends AppCompatActivity {
         textView.setText(newString);
     }
 
+    /**
+     * Setup/populate buttons formating and contents
+     */
     private void populateButtons() {
         // Get the table containing the buttons
         TableLayout table = findViewById(R.id.tlButtons);
@@ -79,7 +97,7 @@ public class GameActivity extends AppCompatActivity {
                 button.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 1.0f));
 
                 // Debug
-                button.setText("" + col + "," + row);
+//                button.setText("" + col + "," + row);
 
                 // Stop text from clipping on smaller buttons
                 button.setPadding(0, 0, 0, 0);
@@ -106,7 +124,69 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Updates every scanned button to show the new number of undiscovered mines
+     */
+    private void updateScannedButtons() {
+        // Iterate through every button, update their scanned text
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COLUMNS; col++) {
+                // If a button has been scanned, update the number of mines inside it
+                if (isButtonScanned(row, col)) {
+                    Button button = buttons[row][col];
+                    int mineCount = getMinesFromCell(row, col);
+                    button.setText("" + mineCount);
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks if a cell is a mine
+     * @param row Row of the cell
+     * @param col Column of the cell
+     * @return True/False if the cell is a mine or not
+     */
+    private boolean isButtonMine(int row, int col) {
+        // Todo: get if the button is a mine through the game model
+        return false;
+    }
+
+    /**
+     * Checks if a cell has been scanned
+     * @param row Row of the cell
+     * @param col Column of the cell
+     * @return True/False if the cell has been scanned
+     */
+    private boolean isButtonScanned(int row, int col) {
+        // Todo: get if the button has been scanned through game model
+        return false;
+    }
+
+    /**
+     * Gets the adjacent mines from a cell
+     * @param row Row of the cell
+     * @param col Column of the cell
+     * @return Number of mines in the cell's row/column
+     */
+    private int getMinesFromCell(int row, int col) {
+        // Todo: get the number of mines in adjacent cells through game model
+        return -1;
+    }
+
+
+
+    /**
+     * Handle a grid button being clicked
+     * @param col The column the button was clicked in
+     * @param row The row the button was clicked in
+     */
     private void gridButtonClicked(int col, int row) {
+        // Do nothing if the button has already been scanned
+        if (isButtonScanned(col, row)) {
+            return;
+        }
+
         // Debug toast
         Toast.makeText(this, "Button clicked: " + col + "," + row, Toast.LENGTH_SHORT).show();
 
@@ -116,22 +196,45 @@ public class GameActivity extends AppCompatActivity {
         // Lock Button Sizes:
         lockButtonSizes();
 
-        // Get the size of the button
-        int width = button.getWidth();
-        int height = button.getHeight();
+        // If the button is unscaned, reveal the mine
+        if (isButtonMine(row, col) && !isButtonScanned(row, col)) {
+            // Get the size of the button
+            int width = button.getWidth();
+            int height = button.getHeight();
 
-        // Scale the bitmap using a bitmap factory
-        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.action_tapped);
-        Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, width, height, true);
+            // Scale the bitmap using a bitmap factory
+            Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.action_tapped);
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, width, height, true);
 
-        // Import the resource
-        Resources resource = getResources();
-        button.setBackground(new BitmapDrawable(resource, scaledBitmap));
+            // Import the resource
+            Resources resource = getResources();
+            button.setBackground(new BitmapDrawable(resource, scaledBitmap));
+        } else {
+            performScan(row, col);
+        }
 
         // Debug clear the button text
-        button.setText("Tapped!");
+        button.setText("5");
+
+        // Refresh the scanned results
+        updateScannedButtons();
     }
 
+    /**
+     * Marks a cell as scanned
+     * @param row Row of the cell that was scanned
+     * @param col Column of the cell that was scanned
+     */
+    private void performScan(int row, int col) {
+        scans ++;
+
+        // Todo: Game logic here performing a scan
+    }
+
+    /**
+     * Locks the size of a button to avoid changing scales when an image is
+     * loaded/contents are changed
+     */
     private void lockButtonSizes() {
         // Iterate through every button, lock their sizes
         for (int row = 0; row < NUM_ROWS; row++) {
