7c8df07bed060256cd29232866911aae432758c7
Yejun Kang <yka92@sfu.ca>
2022-10-10 02:47:37 -0700

    Implement save button and save button logic.

--- app/src/main/java/ca/cmpt276/assignment3/activities/OptionsMenuActivity.java
+++ app/src/main/java/ca/cmpt276/assignment3/activities/OptionsMenuActivity.java
@@ -3,19 +3,75 @@ package ca.cmpt276.assignment3.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 import ca.cmpt276.assignment3.R;
+import ca.cmpt276.assignment3.model.GameOptions;
 
 public class OptionsMenuActivity extends AppCompatActivity {
+
+    // Maybe create a dialog saying that if options were changed and save wasn't clicked and user tries exiting
 
     // 5.3 - Use SharedPreferences to save options data between application runs
     // Use Google Gson for this? Game proprties may use LocalDateTime which requires Google Gson for TypeAdapter.
 
     // 5.4 - Button for resetting game data for each configuration
 
+    private GameOptions gameOptions;
+    private RadioGroup rgBoardSize;
+    private RadioGroup rgMineCount;
+    private int rbSelectedBoardSizeId;
+    private int rbSelectedMineCountId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_menu);
+
+        gameOptions = GameOptions.getInstance();
+        rgBoardSize = findViewById(R.id.rgBoardSize);
+        rgMineCount = findViewById(R.id.rgMineCount);
+        setupSaveButton();
+
+        // When the options screen is opened, setup the last checked options
+        getGameOptions();
+
     }
+
+    private void setupSaveButton(){
+        Button btnSaveOptions = findViewById(R.id.btnSaveOptions);
+        btnSaveOptions.setOnClickListener( view -> {
+            saveGameOptions();
+        });
+    }
+
+    private void getGameOptions(){
+        int boardSizeOption = gameOptions.getSelectedBoardSizeOptionId();
+        int mineCountOption = gameOptions.getSelectedMineCountOptionId();
+
+        // if either one is set to -1, that means default options have not been saved yet.
+        if (boardSizeOption == -1 || mineCountOption == -1){
+            // If they have not been set, just set the id's to the default selected option
+            rbSelectedBoardSizeId = rgBoardSize.getCheckedRadioButtonId();
+            rbSelectedMineCountId = rgMineCount.getCheckedRadioButtonId();
+            return;
+        }
+
+        // Set the ID's
+        rbSelectedBoardSizeId = boardSizeOption;
+        rbSelectedMineCountId = mineCountOption;
+        // Then set the correlated radio button as checked
+        rgBoardSize.check(rbSelectedBoardSizeId);
+        rgMineCount.check(rbSelectedMineCountId);
+    }
+
+    private void saveGameOptions(){
+        // Grab the id's of the selected radio buttons
+        // Pass them into the game options to save
+        rbSelectedBoardSizeId = rgBoardSize.getCheckedRadioButtonId();
+        rbSelectedMineCountId = rgMineCount.getCheckedRadioButtonId();
+        gameOptions.setGameOptions(rbSelectedBoardSizeId, rbSelectedMineCountId);
+    }
+
 }
--- app/src/main/java/ca/cmpt276/assignment3/model/GameOptions.java
+++ app/src/main/java/ca/cmpt276/assignment3/model/GameOptions.java
@@ -3,11 +3,17 @@ package ca.cmpt276.assignment3.model;
 // A class for storing the game options
 public class GameOptions {
 
+    private int selectedBoardSizeOptionId;
+    private int selectedMineCountOptionId;
+
     // Singleton support for options
     private static GameOptions instance;
     private GameOptions() {
         // Private to prevent anything else instantiating this
-        // Setting the default values for the options
+
+        // Set options to -1 when instance is created for the first time
+        selectedBoardSizeOptionId = -1;
+        selectedMineCountOptionId = -1;
     }
     public static GameOptions getInstance(){
         if (instance == null){
@@ -16,4 +22,15 @@ public class GameOptions {
         return instance;
     }
 
+    public void setGameOptions(int selectedBoardSizeOption, int selectedMineCountOption){
+        this.selectedBoardSizeOptionId = selectedBoardSizeOption;
+        this.selectedMineCountOptionId = selectedMineCountOption;
+    }
+
+    public int getSelectedBoardSizeOptionId() {
+        return selectedBoardSizeOptionId;
+    }
+    public int getSelectedMineCountOptionId(){
+        return selectedMineCountOptionId;
+    }
 }
